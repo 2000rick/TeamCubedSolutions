6cebe3ccd019073af891b03c922c01598fb83063
raymondwzeng <raymondwzeng@github.com>
2023-03-23 22:58:24 -0700

    Add checks to make sure that the log path is as intended

--- logging.js
+++ logging.js
@@ -8,6 +8,15 @@ let logPath = `${homeDir}\\Documents\\${"KeoghLongBeach"+currentYear+".txt"}`;
 async function GetDateTime() {
     let apiData = await fetch("http://worldtimeapi.org/api/timezone/America/Los_Angeles");
     let data = await apiData.json();
+    const monthAndDate = data['datetime'].substring(0,10);
+    const year = Number(monthAndDate.substring(0,4));
+    const month = Number(monthAndDate.substring(5,7));
+    const date = Number(monthAndDate.substring(8,10));
+    if (month == 12 && date > 25) { // First, check if we have opened the program during the "going dark" time. In this case, 
+        currentYear++;
+    } else if (year != currentYear) { //If it is not the "going dark" period, just make sure that the year is correct for the sake of the path.
+        currentYear = year;
+    }
     datetime = new Date(data['datetime'].substring(0, 19)).toString();
     let result = datetime.toString().substr(4,11) + ':' + datetime.toString().substr(15, 6) + ' ' + data['abbreviation'];
     return result;
@@ -17,11 +26,6 @@ class Logger {
     // https://www.geeksforgeeks.org/node-js-fs-chmod-method/
     async writeToFile(text) {
         const datetime = await GetDateTime();
-        const year = datetime.substring(7,11);
-        if(year != currentYear) {
-            console.log("Log points to previous year's log. Updating date and logpath.");
-            currentYear++;
-        }
         text = datetime + ' ' + text + "\n";
         if(fs.existsSync(logPath)) {
             fs.chmodSync(logPath, 0o600); //Read/Write
